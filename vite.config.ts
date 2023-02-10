a1e42e82983498a3a54c4522126dccbd02fa7cab
yyhhenry <yyhhenry@foxmail.com>
2023-02-10 11:52:37 +0800

    配置修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import topLevelAwait from 'vite-plugin-top-level-await';
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), topLevelAwait()],
     root: 'src/vue',
     base: '',
     build: {
